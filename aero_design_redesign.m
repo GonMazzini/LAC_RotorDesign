941edb68b930765cc8b6363a01004890f521f8cc
asierbarbeito <90773801+asierbarbeito@users.noreply.github.com>
2021-09-21 17:31:25 +0200

    Polynomials and compare old and new design

--- aero_design_redesign.m
+++ aero_design_redesign.m
@@ -1,5 +1,13 @@
 clear all; close all; clc
 
+load('polynomials.mat');
+load('bladedat.txt');
+olddesign.r = bladedat(:,1);
+olddesign.beta = bladedat(:,2);
+olddesign.c = bladedat(:,3);
+olddesign.tc = bladedat(:,4);clear bladedat
+olddesign.t = olddesign.tc.*olddesign.c/100;
+
 rotor.R = 97.7;
 rotor.tsr = 8.0;
 rotor.B = 3;
@@ -15,9 +23,9 @@ lb = [0, 0]; % lower bounds
 ub = [inf, 1]; % upper bounds
 
 for i = 1:length(rotor.r_lst)
-    res = lsqnonlin(@(x)residuals(x, rotor, i, 0), x0, lb, ub);
+    res = lsqnonlin(@(x)residuals(x, rotor,polynomials, i, 0), x0, lb, ub);
     x0 = [res(1), res(2)];
-    rotor = residuals(x0, rotor, i, 1);
+    rotor = residuals(x0, rotor,polynomials, i, 1);
 end
  
 rotor.c(1:5)
@@ -36,25 +44,65 @@ rotor.CT = trapz(rotor.r_lst, rotor.ct.*rotor.r_lst')*2/rotor.R^2;
 
 figure
 plot(rotor.r_lst, rotor.c)
+title('Chord distribution along blade length')
+xlabel('r [m]')
+ylabel('Chord [m]')
 grid on
 
 figure
 plot(rotor.r_lst, rotor.cp)
+title('Power Coefficient along blade length')
+xlabel('r [m]')
+ylabel('$C_p$')
 grid on
 
+%% Comparison of designs
+
+figure
+subplot(2,2,1)
+plot(rotor.r_lst, rotor.c);hold on;
+plot(olddesign.r,olddesign.c)
+title('Chord distribution')
+xlabel('r [m]');ylabel('c [m]')
+legend('Redesigned','Old')
+grid on; box on;
+
+subplot(2,2,2)
+plot(rotor.r_lst, rotor.beta);hold on;
+plot(olddesign.r,olddesign.beta)
+title('Twist distribution')
+xlabel('r [m]');ylabel('$\beta [deg]$')
+legend('Redesigned','Old')
+grid on; box on;
+
+subplot(2,2,3)
+plot(rotor.r_lst, 100*rotor.t./rotor.c);hold on;
+plot(olddesign.r,olddesign.tc)
+title('Relative thickness distribution')
+xlabel('r [m]');ylabel('$t/c [\%]$')
+legend('Redesigned','Old')
+grid on; box on;
+
+subplot(2,2,4)
+plot(rotor.r_lst, rotor.t);hold on;
+plot(olddesign.r,olddesign.t)
+title('Absolute thickness distribution')
+xlabel('r [m]');ylabel('t [m]')
+legend('Redesigned','Old')
+grid on; box on;
 
 %% F U N C T I O N S
 
-function output = residuals(x, struct, idx, mode)
+function output = residuals(x, struct,struct2, idx, mode)
 
     c = x(1);
     ap = x(2);
     r = struct.r_lst(idx);
 
-    t = thickness(r);
+    t = thickness(r,struct2);
     tcratio = t/c*100;
-    clcd = clcd_des(tcratio);
-    cl = cl_des(tcratio);
+    clcd = clcd_des(tcratio,struct2);
+    cl = cl_des(tcratio,struct2);
     
     % intermediate variables
     phi = atan((1-struct.a)*struct.R/((1+ap)*r*struct.tsr));
@@ -77,8 +125,8 @@ function output = residuals(x, struct, idx, mode)
         struct.t(idx) = t;
         struct.c(idx) = c;
         struct.phi(idx) = phi;
-        struct.alpha(idx) = alpha_des(tcratio);
-        struct.beta(idx) = phi-alpha_des(tcratio);
+        struct.alpha(idx) = alpha_des(tcratio,struct2);
+        struct.beta(idx) = phi-alpha_des(tcratio,struct2);
         struct.cl(idx) = cl;
         struct.cd(idx) = cd;
         struct.ap(idx) = ap;
@@ -89,17 +137,17 @@ function output = residuals(x, struct, idx, mode)
     end
 end
 
-function t = thickness(r)
+function t = thickness(r,polynomials)
     %Absolute thickness [m] as a function of radius [m] for 35-m blade
     if r<=5
         r = 5;
     end
-    t = -1.000521397083093e-10*r^6 + 3.827600673875859e-08*r^5 - 5.799931472937034e-06*r^4 ...
-        + 4.275676654471356e-04*r^3 - 0.014369744235553*r^2 + 0.077853410396680*r + 5.796361265081653;
+    p = polynomials.t;
+    t = polyval(p,r);
 end
 
 
-function alpha = alpha_des(tcratio)
+function alpha = alpha_des(tcratio,polynomials)
     %Design AOA [deg] as a function of t/c [%]
 
     if tcratio<24
@@ -110,17 +158,18 @@ function alpha = alpha_des(tcratio)
     end
 
     if tcratio < 48
-        alpha = 0.002634259259259*tcratio^3 - 0.281034722222214*tcratio^2 ...
-            + 9.382141666666378*tcratio -90.387299999996500;
+        p = polynomials.alpha1;
+        alpha = polyval(p,tcratio);        
     elseif 36 <= tcratio
-        alpha = -0.072682692307692*tcratio + 7.268269230769232;
+        p = polynomials.alpha2;
+        alpha = polyval(p,tcratio);
     else
         print('Invalid t/c input')
     end
 end
 
 
-function cl = cl_des(tcratio)
+function cl = cl_des(tcratio,polynomials)
     %Design cl [-] as a function of t/c [%]
 
     if tcratio<24
@@ -131,17 +180,18 @@ function cl = cl_des(tcratio)
     end
 
     if tcratio < 48
-        cl = -1.013773148148117e-04*tcratio^3 + 0.009146458333333*tcratio^2 ...
-            -0.274282499999987*tcratio + 4.146159999999838;
+        p = polynomials.cl1;
+        cl = polyval(p,tcratio);
     elseif 48 <= tcratio
-        cl = -0.016202307692308*tcratio + 1.620230769230769;
+        p = polynomials.cl2;
+        cl = polyval(p,tcratio);
     else
         print('Invalid t/c range')
     end
 
 end
 
-function clcd = clcd_des(tcratio)
+function clcd = clcd_des(tcratio,polynomials)
     %Design cl/cd [-] as a function of t/c [%]
 
     if tcratio<24
@@ -152,10 +202,11 @@ function clcd = clcd_des(tcratio)
     end
     
     if tcratio < 48
-        clcd = -0.002717789431521*tcratio^3 + 0.287987668161641*tcratio^2 ...
-            - 13.087292916483413*tcratio + 2.900341253728703e+02;
+        p = polynomials.clcd1;
+        clcd = polyval(p,tcratio);
     elseif 48 <= tcratio
-        clcd = -0.476959307986685*tcratio + 47.695930798668506;
+        p = polynomials.clcd2;
+        clcd = polyval(p,tcratio);         
     else
         print('Invalid t/c range')
     end
