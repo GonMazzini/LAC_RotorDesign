fc5fd3b9e5fd8d791078f5ee1022ddad0059b616
GonMazzini <gmazzini@itba.edu.ar>
2021-09-26 14:06:05 +0200

    add subplots for spanwise

--- post_processing.m
+++ post_processing.m
@@ -8,7 +8,7 @@ set(groot, 'defaultLegendInterpreter','latex');
 set(0,'defaultAxesFontSize',12);
 set(0, 'DefaultLineLineWidth', 1);
 set(0, 'DefaultFigureRenderer', 'painters');
-set(0,'DefaultFigureWindowStyle','docked')
+set(0,'DefaultFigureWindowStyle','normal')
 
 
 %% 
@@ -73,44 +73,71 @@ name = {'$\alpha$ [deg]' '$C_l$ [-]', '$C_l/C_d$ [-]'};
 pos = [5, 17, 18];
 desired_TSR = 6.75; %Choose desired TSR value
 
-tcratio = 14:100;
-[alpha, cl, clcd] = deal(zeros(length(tcratio), 1));
-for i=1:length(tcratio)
-    alpha(i,1) = alpha_des(tcratio(i), polynomials);
-    cl(i,1) = cl_des(tcratio(i), polynomials);
-    clcd(i,1) = clcd_des(tcratio(i), polynomials);
+% tcratio = 14:100;
+% for i=1:length(tcratio)
+%     alpha(i,1) = alpha_des(tcratio(i), polynomials);
+%     cl(i,1) = cl_des(tcratio(i), polynomials);
+%     clcd(i,1) = clcd_des(tcratio(i), polynomials);
+% end
+[tc, alpha, cl, clcd] = deal(zeros(length(data(:,1)), 1));
+
+
+idx = find(TSR == desired_TSR);
+filename = sprintf(append(prefix,'_u800%d.ind'), idx-1);
+data = readtable(filename,'Filetype', 'text');
+data = table2array(data);
+r = data(:,1);
+for k=1:length(data(:,1))
+    t = thickness(data(k,1), polynomials);
+    if t>5.38
+        t = 5.38;
+    end
+    
+    tc(k,1) = t/data(k,32)*100;
+
+    if tc(k,1) < 24.1
+        tc(k,1) = 24.1;
+    end
+      alpha(k,1) = alpha_des(tc(k,1), polynomials);
+      cl(k,1) = cl_des(tc(k,1), polynomials);
+      clcd(k,1) = clcd_des(tc(k,1), polynomials);
 end
+
 
 
 for i=1:length(pos)
     figure;
-    idx = find(TSR == desired_TSR);
-    filename = sprintf(append(prefix,'_u800%d.ind'), idx-1);
-    data = readtable(filename,'Filetype', 'text');
-    data = table2array(data);
-    tc = zeros(length(data(:,1)),1);
-    for k=1:length(data(:,1))
-        t = thickness(data(k,1), polynomials);
-        if t>5.38
-            t = 5.38;
-        end
-        tc(k,1) = t/data(k,32)*100;
-        if tc(k,1) < 24.1
-            tc(k,1) = 24.1;
-        end
-    end
-    if pos(i) == 5
+    subplot(2,1,1)
+    
+%     tc = zeros(length(data(:,1)),1);
+    
+
+   
+    if pos(i) == 5 % alpha
         plot(tc, rad2deg(data(:,pos(i))), 'DisplayName', 'actual');
         hold on
-        plot(tcratio, alpha, 'DisplayName', 'design');
-    elseif pos(i) == 18
+        plot(tc, alpha, 'DisplayName', 'design');
+        subplot(2,1,2)
+        plot(r, rad2deg(data(:,pos(i))), 'DisplayName', 'actual');
+        hold on
+        plot(r, alpha, 'DisplayName', 'design');
+        
+    elseif pos(i) == 18 % cl/cd
         plot(tc, data(:,pos(i)-1)./data(:,pos(i)), 'DisplayName', 'actual');
         hold on
-        plot(tcratio, clcd, 'DisplayName', 'design');
-    else
+        plot(tc, clcd, 'DisplayName', 'design');
+        subplot(2,1,2)
+        plot(r, data(:,pos(i)-1)./data(:,pos(i)), 'DisplayName', 'actual');
+        hold on
+        plot(r, clcd, 'DisplayName', 'design');
+    elseif pos(i) == 17 % cl 
         plot(tc, data(:,pos(i)), 'DisplayName', 'actual');
         hold on
-        plot(tcratio, cl, 'DisplayName', 'design');
+        plot(tc, cl, 'DisplayName', 'design');
+        subplot(2,1,2)
+        plot(r, data(:,pos(i)), 'DisplayName', 'actual');
+        hold on
+        plot(r, cl, 'DisplayName', 'design');
     end
     
 
@@ -118,7 +145,7 @@ for i=1:length(pos)
     xlabel('t/c [\%]');
     ylabel(name(i));
     legend;
-    xlim([min(tcratio), max(tcratio) ])
+%     xlim([min(tcratio), max(tcratio) ])
 end
 
 
