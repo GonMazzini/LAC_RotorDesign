a99d622d33bdf55bb8240b0873fb97b3909564ec
mathieupelle <mathieu.pelle7@gmail.com>
2021-10-30 15:57:30 +0200

    Updated for case C7 and saving figures.

--- Control/load_hawc2.py
+++ Control/load_hawc2.py
@@ -44,7 +44,9 @@ cwd = os.getcwd()
 cwd = cwd.replace('\\','/')
 folder = cwd + '/results/'
 
-cases = ['C2', 'C3'] #Add cases you want to plot
+#cases = ['C1', 'C2', 'C3'] #Add cases you want to plot
+
+cases = ['C4', 'C5', 'C6']
 
 data_lst = []
 for c in range(len(cases)):
@@ -59,30 +61,84 @@ irotspd = 9  # column index of rotor speed
 iwspd = 14 # column index of wind speed
 ipitch = 70 # column index of pitch angle
 iepower = 99 # column index of electrical power
+itorque = 69 # column index of generator torque
 
+data_all = data_lst
+time_shift = 0
 # ====================== plot things for fun ======================
-idx = [irotspd, iwspd, ipitch, iepower]
-leg = ['$\omega$ [rad/s]', '$V_{\infty}$ [m/s]', r'$\theta$ [deg]', '$P_{electric}$ [MW]']
-for i in range(4):
+idx = [irotspd, iwspd, ipitch, iepower, itorque]
+name = ['rpm', 'wspd', 'pitch', 'power', 'torque']
+leg = ['$\omega$ [rad/s]', '$V_{\infty}$ [m/s]', r'$\theta$ [deg]',
+       '$P_{electric}$ [MW]', '$Q_{gen}$ [Nm]']
+#lab = ['C1: $f = 0.05$, $\zeta = 0.7$', 'C2: $f = 0.01$, $\zeta = 0.7$', 'C3: $f = 0.10$, $\zeta = 0.7$']
+lab = ['C4: $f = 0.05$, $\zeta = 0.7$', 'C5: $f = 0.01$, $\zeta = 0.7$','C6: $f = 0.10$, $\zeta = 0.7$']
+
+
+for i in range(len(idx)):
     plt.figure()
-    for j in range(len(cases)):
-        data = data_lst[j]
-        time = data[:, itime]
-        y = data[:, idx[i]]
+    for j in range(len(data_all)):
+        data = data_all[j]
+        time = data[time_shift*100:, itime]
+        y = data[time_shift*100:, idx[i]]
         if idx[i] == ipitch:
             y = np.rad2deg(y)
         elif idx[i] == iepower:
             y = y/10**6
-        plt.plot(time, y, label=cases[j])
+        plt.plot(time, y, label=lab[j])
     plt.ylabel(leg[i])
     plt.xlabel('t [s]')
     plt.grid()
     plt.legend()
+    #plt.savefig('figures/cstP_'+name[i]+'.png')
+    plt.savefig('figures/cstT_'+name[i]+'.png')
 
 
 
+#%% C7: C1 Improvement
+
+cases = ['C1', 'C7_f05_zeta06', 'C7_f035_zeta06']
+data_lst = []
+for c in range(len(cases)):
+    path = folder + 'dtu_10mw_rwt_' + cases[c] + '.dat'
+    data = load_hawc_ascii(path)
+    data_lst.append(data)
 
 
+#%%
 
+itime = 0  # column index of time
+irotspd = 9  # column index of rotor speed
+iwspd = 14 # column index of wind speed
+ipitch = 70 # column index of pitch angle
+iepower = 99 # column index of electrical power
+itorque = 95 # column index of generator torque
+ithrust = 11 # column index of generator torque
 
+data_all = data_lst
+lab = ['C1: $f = 0.05$, $\zeta = 0.7$', 'C7: $f = 0.05$, $\zeta = 0.6$', 'C7: $f = 0.035$, $\zeta = 0.6$']
+time_start = 350
+time_end = 500
+# ====================== plot things for fun ======================
+idx = [irotspd, iwspd, ipitch, iepower, itorque, ithrust]
+name = ['rpm', 'wspd', 'pitch', 'power', 'torque', 'thrust']
+leg = ['$\omega$ [rad/s]', '$V_{\infty}$ [m/s]', r'$\theta$ [deg]',
+       '$P_{electric}$ [MW]', '$Q_{gen}$ [Nm]', 'T [kN]']
+for i in range(len(idx)):
+    plt.figure()
+    for j in range(len(data_all)):
+        data = data_all[j]
+        time = data[time_start*100:time_end*100, itime]-time_shift
+        y = data[time_start*100:time_end*100, idx[i]]
+        if idx[i] == ipitch:
+            y = np.rad2deg(y)
+        elif idx[i] == iepower:
+            y = y/10**6
+        elif idx[i] == ithrust:
+            y = y/1000
+        plt.plot(time, y, label=lab[j])
+    plt.ylabel(leg[i])
+    plt.xlabel('t [s]')
+    plt.grid()
+    plt.legend()
+    plt.savefig('figures/part2_'+name[i]+'.png')
 
